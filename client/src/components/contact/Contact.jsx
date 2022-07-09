2cd7cf9adf4aef42f3e4f572b08ee76e5c320dde
Anlon Zhu <zhu.anlon@gmail.com>
2022-07-09 13:47:07 -0700

    contact form update 2

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import "./contact.scss"
 import React from 'react';
 import { useState } from "react";
+<<<<<<< HEAD
 import axios from 'axios';
 
 export default function Contact() {
@@ -63,6 +64,17 @@ export default function Contact() {
         });
     };
 
+=======
+
+export default function Contact() {
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  }
+  
+>>>>>>> bdc5e28 (contact form update 2)
     return(
         <div className="contact">
             <div className="left">
@@ -72,6 +84,7 @@ export default function Contact() {
             <div className="right">
             <h2>Contact</h2>
         
+<<<<<<< HEAD
             <form onSubmit={handleOnSubmit}>
             <label htmlFor="name">Full Name</label>
             <input
@@ -116,3 +129,22 @@ export default function Contact() {
         </div>
       );
 }
+=======
+            <form id="fs-frm" name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/f/xknylvee" onSubmit={handleSubmit} method="post">
+ 
+    <label for="full-name">Full Name</label>
+    <input type="text" name="name" id="full-name" placeholder="First and Last" required=""/>
+    <label for="email-address">Email Address</label>
+    <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required=""/>
+    <label for="message">Message</label>
+    <textarea rows="5" name="message" id="message" placeholder="Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla nullam quis risus." required=""></textarea>
+    <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission"/>
+ 
+  <button type="submit">Send</button>
+  {message && <span>Thank you for your message!</span>}
+</form>
+            </div>
+        </div>
+      );
+}
+>>>>>>> bdc5e28 (contact form update 2)
