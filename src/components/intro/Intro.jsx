283ad0b590e615f6e035175519727799f070f592
Anlon Zhu <zhu.anlon@gmail.com>
2023-03-20 23:18:23 -0400

    updates 2023

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,36 +1,41 @@
-
-import "./intro.scss"
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import "./intro.scss";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import TypeAnimation from "react-type-animation";
- 
 
 export default function Intro() {
-
   return (
     <div className="intro" id="intro">
-        <div className="left">
-          <div className="imgContainer">
-            <img src="assets/headshot.png" alt="" />
-          </div>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/AnlonZ_headshot.png" alt="" />
         </div>
-        <div className="right">
-          <div className="wrapper">
-            
-            <h2>Hello! I'm</h2>
-            <h1>Anlon Zhu</h1>
-            <h3>
-              <TypeAnimation
-                className="type"
-                cursor={false}
-                sequence={['Princeton Computer Science', 2200, 'Software Developer', 2200, 'Sustainability Advocate', 2200, 'Princeton Computer Science', 2200]}
-                wrapper="h3"
-              />
-            </h3>
-          </div>
-          <a href="#portfolio">
-            <KeyboardArrowDownIcon className="arrow"/>
-          </a>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hello! I'm</h2>
+          <h1>Anlon Zhu</h1>
+          <h3>
+            <TypeAnimation
+              className="type"
+              cursor={false}
+              sequence={[
+                "Princeton Computer Science",
+                2200,
+                "Software Developer",
+                2200,
+                "Sustainability Advocate",
+                2200,
+                "Princeton Computer Science",
+                2200,
+              ]}
+              wrapper="h3"
+            />
+          </h3>
         </div>
+        <a href="#portfolio">
+          <KeyboardArrowDownIcon className="arrow" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
